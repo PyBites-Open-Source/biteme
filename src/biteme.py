4a738e7cb458bdd4410618ed9cd2176f5b789cfe
Will Frey <jfrey89@gmail.com>
2020-10-08 17:37:01 -0400

    ♻️ Refactor _extract

--- src/biteme.py
+++ src/biteme.py
@@ -1,25 +1,48 @@
 from __future__ import annotations
 
-from io import BytesIO
-from os import PathLike
+import io
+import os
+import shutil
+import tempfile
+from contextlib import ExitStack
+from itertools import filterfalse
 from pathlib import Path
 from tempfile import TemporaryDirectory
-from typing import Union
-from zipfile import ZipFile
+from typing import TYPE_CHECKING, BinaryIO, NewType, Union
+from urllib.parse import urljoin
+from zipfile import ZipFile, ZipInfo
 
 import requests
 
 
-StrPath = Union[str, "PathLike[str]"]
+_BiteID = NewType("_BiteID", int)
+
+_StrPath = Union[str, "os.PathLike[str]"]
 
 
-def _extract_python_files(
-    bite_archive: Union[StrPath, BytesIO, ZipFile], target_directory: StrPath
+def _download(bite_id: _BiteID) -> ZipFile:
+    filename = f"pybites_bite{bite_id}.zip"
+    url = urljoin("https://bite-zipfiles.s3.eu-west-3.amazonaws.com", filename)
+    response = requests.get(url)
+    buffer = io.BytesIO(response.content)
+    archive = ZipFile(buffer)
+    archive.filename = filename
+    return archive
+
+
+def _is_macos_resource_fork(member: Union[str, ZipInfo]) -> bool:
+    if isinstance(member, ZipInfo):
+        member = member.filename
+    return member.startswith("__MACOSX/")
+
+
+def _extract(
+    archive: Union[ZipFile, _StrPath, BinaryIO], directory: _StrPath
 ) -> Path:
-    if not isinstance(bite_archive, ZipFile):
-        bite_archive = ZipFile(bite_archive)
+    if not isinstance(archive, ZipFile):
+        archive = ZipFile(archive)
 
-    target_directory = Path(target_directory)
+    directory = Path(directory)
 
     # XXX (Will): Should this be more precise? I want exactly two .py
     # files: "<module-name>.py" and "test_<module-name>.py", for some
@@ -27,38 +50,25 @@ def _extract_python_files(
     # made on macOS, I'll have a one-to-one correspondance between the
     # actual archive members found in "<archive-name>/" and the archive
     # members found in "__MACOSX/.<archive-name>/".
-    python_module_names = [
-        name
-        for name in bite_archive.namelist()
-        if not name.startswith("__MACOSX/") and name.endswith(".py")
-    ]
-    if len(python_module_names) != 2:
-        # TODO (Will): Write a more helpful error message.
-        raise RuntimeError("unrecognized bite archive")
-
-    # XXX (Will): Would using `shutil.copyfileobj` with
-    # `ZipFile.read(...)` be preferable here?
-    bite_archive.extractall(target_directory, members=python_module_names)
-
-    for path in (target_directory / name for name in python_module_names):
-        path.rename(target_directory / path.name)
-
-    for subdirectory in filter(Path.is_dir, target_directory.iterdir()):
-        subdirectory.rmdir()
+    for member in filterfalse(
+        lambda member: member.is_dir() or _is_macos_resource_fork(member),
+        archive.infolist(),
+    ):
+        target_path = directory / os.path.basename(member.filename)
+        with archive.open(member) as source, target_path.open("wb") as target:
+            shutil.copyfileobj(source, target, member.file_size)
 
     return directory
 
 
 if __name__ == "__main__":
-    url = "https://bite-zipfiles.s3.eu-west-3.amazonaws.com/pybites_bite1.zip"
-    response = requests.get(url)
-    archive = BytesIO(response.content)
+    bite_id = _BiteID(1)
+    archive = _download(bite_id)
 
-    with TemporaryDirectory() as directory:
-        directory = Path(directory)
+    directory = Path(__file__).parents[1] / f"{bite_id}"
+    directory.mkdir()
 
-        _extract_python_files(archive, directory)
+    _extract(archive, directory)
 
-        assert len(list(directory.iterdir())) == 2
-        for child in directory.iterdir():
-            print(f"{child}")
+    for child in directory.iterdir():
+        print(f"{child}")
